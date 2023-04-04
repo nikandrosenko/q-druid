a1493942087f1247adf4d086bfefb245b89b1f58
Andrew Nasibullin <duhich02@yandex.ru>
2023-04-04 14:25:51 +0300

    Ввел логи для просмотра данных/поиска ошибок

--- src/pages/GroupPage.vue
+++ src/pages/GroupPage.vue
@@ -8,22 +8,14 @@
       <q-markup-table>
         <thead>
         <tr>
-          <th class="text-left">Dessert (100g serving)</th>
-          <th class="text-right">Calories</th>
-          <th class="text-right">Fat (g)</th>
-          <th class="text-right">Carbs (g)</th>
-          <th class="text-right">Protein (g)</th>
-          <th class="text-right">Sodium (mg)</th>
+          <th class="text-left">Участник</th>
+          <th class="text-right">Вторая колонна</th>
         </tr>
         </thead>
-        <tbody v-for="user in getGroupSubjects.get_group" :key="user.id">
+        <tbody v-for="user in subjects" :key="user.id">
         <tr>
-          <td>{{ user.FIO }}</td>
-          <td>{{ user.phone }}</td>
-          <td>{{ user.email }}</td>
-          <td>{{ user.name }}</td>
-          <td>{{ user.age }}</td>
-          <td>{{ user.sex }}</td>
+          <td>{{ user.subject.fullname.first_name }}</td>
+          <td>{{ user.subject.fullname.last_name }}</td>
         </tr>
         </tbody>
       </q-markup-table>
@@ -37,45 +29,18 @@ import gql from 'graphql-tag';
 import { computed } from "vue";
 import { getPage, getGroupSubjects } from "src/graphql/queries";
 
-const childrenPages = '';
-const { result, onResult } = useQuery(gql`
-      query {
-        page(id: "6932152816939161311") {
-          id
-          parent_id
-          page_type
-          title
-          content
-          icon
-          level
-          is_public
-          position
-          config
-          is_block
-          children(perPage: 10, page: 1) {
-            data {
-              id
-              title
-            }
-          }
-          created_at
-          updated_at
-        }
-      }
-    `);
-onResult(() => {
-  childrenPages.value = result.value.page.title;
-  console.log(childrenPages.value);
-});
-
 
 const { result: page } = useQuery(getPage, {
   id: id,
 });
+console.log(page.id)
+
 const subjectId = computed(() => page.value.page.object.id);
+console.log(subjectId)
 
 const { result: subjects } = useQuery(getGroupSubjects, {
   group_id: subjectId,
 });
+console.log(group_id)
 
 </script>
