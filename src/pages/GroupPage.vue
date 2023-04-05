0446ee9328eef31f50c5297d628aa6e9289f0ccf
Andrew Nasibullin <duhich02@yandex.ru>
2023-04-05 16:54:29 +0300

    исправленна ошибка

--- src/pages/GroupPage.vue
+++ src/pages/GroupPage.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="q-pa-md">
+<!--    <pre>{{ page }}</pre>-->
     <q-card class="my-card" flat bordered>
       <q-card-section>
-        <div class="text-h6">{{ page.title }}</div>
+        <div class="text-h6">{{ page?.page.title }}</div>
         <div class="text-subtitle2">by John Doe</div>
       </q-card-section>
       <q-markup-table>
@@ -23,22 +24,18 @@
   </div>
 </template>
 <script setup>
-import { reactive, onMounted } from 'vue';
 import { useQuery } from '@vue/apollo-composable';
-import gql from 'graphql-tag';
-import { computed } from "vue";
 import { getPage, getGroupSubjects } from "src/graphql/queries";
 
-const group_id = 6579648768563146067;
-const id = 5901342623205784345;
+const group_id = '6579648768563146067';
+const id = '5901342623205784345';
 
 const { result: page } = useQuery(getPage, {
   id: id});
-console.log(page.id)
-
-const { result: subjects } = useQuery(getGroupSubjects, {
-  group_id: page?.subject?.id,
-});
-console.log(group_id)
+// console.log(page.id)
+// const { result: subjects } = useQuery(getGroupSubjects, {
+//   group_id: page?.subject?.id,
+// });
+// console.log(group_id)
 
 </script>
