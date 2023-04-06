0f65a26e2bd77ae69cb8f428346487b7fc7f85de
Nikita Androsenko <varzaid@gmail.com>
2023-04-06 19:15:58 +0300

    Добавлены мутации

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -95,3 +95,189 @@ export const userUpdate = gql`
     }
   }
 `;
+export const createModule = gql`
+  mutation ($input: create_type1_input!) {
+    create_type1(input: $input) {
+      status
+      recordId
+      record {
+        id
+        type_id
+        author_id
+        level
+        position
+        created_at
+        updated_at
+        name
+        property5 {
+          id
+          user_id
+          fullname {
+            first_name
+            last_name
+          }
+        }
+        property6 {
+          date
+          time
+        }
+        property7 {
+          date
+          time
+        }
+        property4 {
+          name
+          property1
+          property2 {
+            fullname {
+              first_name
+              last_name
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+export const createTask = gql`
+  mutation ($input: create_type2_input!) {
+    create_type2(input: $input) {
+      status
+      recordId
+      record {
+        id
+        type_id
+        author_id
+        level
+        position
+        created_at
+        updated_at
+        name
+        property1
+        property2 {
+          id
+          user_id
+          fullname {
+            first_name
+            last_name
+          }
+        }
+        property4 {
+          id
+        }
+      }
+    }
+  }
+`;
+export const createPermissionRule = gql`
+  mutation permissionRuleCreate($input: PermissionRuleCreateInput!) {
+    permissionRuleCreate(input: $input) {
+      recordId
+      status
+      record {
+        id
+        author_id
+        level
+        model_type
+        model_id
+        config
+        owner_id
+        owner_type
+        created_at
+        updated_at
+      }
+    }
+  }
+`;
+export const updateTask = gql`
+  mutation ($id: String!, $input: update_type1_input!) {
+    update_type2(id: $id, input: $input) {
+      status
+      recordId
+      record {
+        id
+        type_id
+        author_id
+        level
+        position
+        created_at
+        updated_at
+        name
+        property1
+        property2 {
+          id
+          user_id
+          fullname {
+            first_name
+            last_name
+          }
+        }
+        property3
+      }
+    }
+  }
+`;
+export const createPage = gql`
+  mutation ($input: PageCreateInput!) {
+    pageCreate(input: $input) {
+      recordId
+      status
+      record {
+        parent_id
+        page_type
+        title
+        content
+        icon
+        level
+        is_public
+        position
+        config
+        created_at
+        updated_at
+        object {
+          id
+          type_id
+        }
+      }
+    }
+  }
+`;
+export const updateModule = gql`
+  mutation ($input: update_type1_input!, $id: String!) {
+    update_type1(id: $id, input: $input) {
+      status
+      recordId
+      record {
+        id
+        type_id
+        author_id
+        level
+        position
+        created_at
+        updated_at
+        name
+      }
+    }
+  }
+`;
+export const deleteModule = gql`
+  mutation deleteModule($module_id: String!) {
+    delete_type1(id: $module_id) {
+      recordId
+    }
+  }
+`;
+export const deleteTask = gql`
+  mutation deleteTask($task_id: String!) {
+    delete_type2(id: $task_id) {
+      recordId
+    }
+  }
+`;
+export const deletePage = gql`
+  mutation deletePage($page_id: String!) {
+    pageDelete(id: $page_id) {
+      recordId
+    }
+  }
+`;
