a79e9835e1a9b9476bfd9e6cdb2e3518ba7507da
Andrew Nasibullin <duhich02@yandex.ru>
2023-04-07 00:46:45 +0300

    Почистил, удалил комментарии. Должно работать...

--- src/components/GroupPage.vue
+++ src/components/GroupPage.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="q-pa-md">
-
-
-
     <div class="q-pa-md q-gutter-sm">
       <q-btn label="Пригласить участника" color="primary" @click="prompt = true" />
       <q-dialog v-model="prompt" persistent>
@@ -28,70 +25,6 @@
         </q-card>
       </q-dialog>
     </div>
-
-
-
-<!--    <q-btn-->
-<!--      @click="inviteUser = true"-->
-<!--      style="background: #ef7540; color: white; border-radius: 25px"-->
-<!--      flat-->
-<!--      label="Пригласить"-->
-<!--      v-close-popup-->
-<!--    />-->
-<!--    <q-card v-show="inviteUser" style="min-width: 450px; border-radius: 25px">-->
-<!--      <q-card-section>-->
-<!--        <div class="text-h6 text-black" style="color: #fefefe">-->
-<!--          ОСТАВИТЬ ЗАЯВКУ-->
-<!--        </div>-->
-<!--      </q-card-section>-->
-<!--      <q-card-section class="q-pt-none" style="color: #fefefe">-->
-<!--        <q-input-->
-<!--          standout="bg-primary text-white"-->
-<!--          filled-->
-<!--          v-model="req.email"-->
-<!--          label="Почта"-->
-<!--          lazy-rules-->
-<!--          :rules="[(val) => (val && val.length > 0) || 'Please type something']"-->
-<!--        />-->
-<!--        <div-->
-<!--          style="-->
-<!--            display: flex;-->
-<!--            justify-content: space-between;-->
-<!--            margin: 0 0 10px;-->
-<!--          "-->
-<!--        >-->
-<!--          <q-input-->
-<!--            standout="bg-primary text-white"-->
-<!--            filled-->
-<!--            v-model="req.name"-->
-<!--            label="Имя"-->
-<!--            lazy-rules-->
-<!--            :rules="[-->
-<!--              (val) => (val && val.length > 0) || 'Please type something',-->
-<!--            ]"-->
-<!--          />-->
-<!--          <q-input-->
-<!--            standout="bg-primary text-white"-->
-<!--            filled-->
-<!--            v-model="req.surname"-->
-<!--            label="Фамилия"-->
-<!--            lazy-rules-->
-<!--            :rules="[-->
-<!--              (val) => (val && val.length > 0) || 'Please type something',-->
-<!--            ]"-->
-<!--          />-->
-<!--        </div>-->
-<!--      </q-card-section>-->
-<!--      <q-card-actions align="right" class="text-primary">-->
-<!--        <q-btn-->
-<!--          style="background: #ef7540; color: white; border-radius: 25px"-->
-<!--          flat-->
-<!--          label="Пригласить"-->
-<!--          @click="onSubmit"-->
-<!--          v-close-popup-->
-<!--        />-->
-<!--      </q-card-actions>-->
-<!--    </q-card>-->
     <q-card style="border: 1px solid gray" class="my-card" flat bordered>
       <q-card-section>
         <div class="text-h6">{{ page?.page.title }}</div>
@@ -123,10 +56,9 @@ import { computed, ref, onMounted } from "vue";
 import { userGroupInviteUser } from "src/graphql/mutations";
 
 const id = ref("");
-
-const inviteUser = ref(false);
 const route = useRoute();
 const prompt = ref(false);
+
 const defId = () => {
   id.value = route.params.id;
 };
@@ -151,29 +83,10 @@ const req = ref({
   name: "",
   surname: "",
 });
-// const { mutate: userGroupInviteUser } = useMutation(gql`
-//   mutation UserGroupInviteUser($input: UserGroupInviteUserInput!) {
-//     userGroupInviteUser(input: $input) {
-//       status
-//     }
-//   }
-// `);
-// const onSubmit = async () => {
-//   const { data } = await userGroupInviteUser({
-//     input: {
-//       name: req.value.name,
-//       surname: req.value.surname,
-//       email: req.value.email,
-//       page_group_id: id.value,
-//     },
-//   });
-//   console.log(data.userGroupInviteUser.status);
-//   resetForm();
-// };
+
 const resetForm = () => {
   (req.value.email = ""), (req.value.surname = ""), (req.value.name = "");
 };
-
 const { mutate: onSubmit } = useMutation(userGroupInviteUser, {
   input: {
     email: req.value.email,
@@ -182,7 +95,6 @@ const { mutate: onSubmit } = useMutation(userGroupInviteUser, {
     page_group_id: id.value,
   },
 });
-
 onMounted(() => {
   defId();
 });
