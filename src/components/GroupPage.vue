84c3b450b0a6c5b138aeab1c1ebbe7291f757275
Andrew Nasibullin <duhich02@yandex.ru>
2023-04-07 00:31:50 +0300

    Добавил диалоговое окно добавления участника вместо былого немодального окна. Добавил рамочки в таблицу участников

--- src/components/GroupPage.vue
+++ src/components/GroupPage.vue
@@ -1,83 +1,114 @@
 <template>
   <div class="q-pa-md">
-    <q-btn
-      @click="inviteUser = true"
-      style="background: #ef7540; color: white; border-radius: 25px"
-      flat
-      label="Пригласить"
-      v-close-popup
-    />
-    <q-card v-show="inviteUser" style="min-width: 450px; border-radius: 25px">
-      <q-card-section>
-        <div class="text-h6 text-black" style="color: #fefefe">
-          ОСТАВИТЬ ЗАЯВКУ
-        </div>
-      </q-card-section>
-      <q-card-section class="q-pt-none" style="color: #fefefe">
-        <q-input
-          standout="bg-primary text-white"
-          filled
-          v-model="req.email"
-          label="Почта"
-          lazy-rules
-          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-        />
-        <div
-          style="
-            display: flex;
-            justify-content: space-between;
-            margin: 0 0 10px;
-          "
-        >
-          <q-input
-            standout="bg-primary text-white"
-            filled
-            v-model="req.name"
-            label="Имя"
-            lazy-rules
-            :rules="[
-              (val) => (val && val.length > 0) || 'Please type something',
-            ]"
-          />
-          <q-input
-            standout="bg-primary text-white"
-            filled
-            v-model="req.surname"
-            label="Фамилия"
-            lazy-rules
-            :rules="[
-              (val) => (val && val.length > 0) || 'Please type something',
-            ]"
-          />
-        </div>
-      </q-card-section>
-      <q-card-actions align="right" class="text-primary">
-        <q-btn
-          style="background: #ef7540; color: white; border-radius: 25px"
-          flat
-          label="Пригласить"
-          @click="onSubmit"
-          v-close-popup
-        />
-      </q-card-actions>
-    </q-card>
-    <q-card class="my-card" flat bordered>
+
+
+
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn label="Пригласить участника" color="primary" @click="prompt = true" />
+      <q-dialog v-model="prompt" persistent>
+        <q-card style="min-width: 350px">
+          <q-card-section>
+            <div class="text-h6">Пригласить участника</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <q-input square dense v-model="req.name" autofocus placeholder="Имя" @keyup.enter="prompt = false" />
+          </q-card-section>
+          <q-card-section class="q-pt-none">
+            <q-input dense v-model="req.surname" autofocus placeholder="Фамилия" @keyup.enter="prompt = false" />
+          </q-card-section>
+          <q-card-section class="q-pt-none">
+            <q-input dense v-model="req.email" autofocus placeholder="Почта" @keyup.enter="prompt = false" />
+          </q-card-section>
+
+          <q-card-actions align="right" class="text-primary">
+            <q-btn flat label="Отмена" v-close-popup />
+            <q-btn flat label="Пригласить" @click="onSubmit" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+    </div>
+
+
+
+<!--    <q-btn-->
+<!--      @click="inviteUser = true"-->
+<!--      style="background: #ef7540; color: white; border-radius: 25px"-->
+<!--      flat-->
+<!--      label="Пригласить"-->
+<!--      v-close-popup-->
+<!--    />-->
+<!--    <q-card v-show="inviteUser" style="min-width: 450px; border-radius: 25px">-->
+<!--      <q-card-section>-->
+<!--        <div class="text-h6 text-black" style="color: #fefefe">-->
+<!--          ОСТАВИТЬ ЗАЯВКУ-->
+<!--        </div>-->
+<!--      </q-card-section>-->
+<!--      <q-card-section class="q-pt-none" style="color: #fefefe">-->
+<!--        <q-input-->
+<!--          standout="bg-primary text-white"-->
+<!--          filled-->
+<!--          v-model="req.email"-->
+<!--          label="Почта"-->
+<!--          lazy-rules-->
+<!--          :rules="[(val) => (val && val.length > 0) || 'Please type something']"-->
+<!--        />-->
+<!--        <div-->
+<!--          style="-->
+<!--            display: flex;-->
+<!--            justify-content: space-between;-->
+<!--            margin: 0 0 10px;-->
+<!--          "-->
+<!--        >-->
+<!--          <q-input-->
+<!--            standout="bg-primary text-white"-->
+<!--            filled-->
+<!--            v-model="req.name"-->
+<!--            label="Имя"-->
+<!--            lazy-rules-->
+<!--            :rules="[-->
+<!--              (val) => (val && val.length > 0) || 'Please type something',-->
+<!--            ]"-->
+<!--          />-->
+<!--          <q-input-->
+<!--            standout="bg-primary text-white"-->
+<!--            filled-->
+<!--            v-model="req.surname"-->
+<!--            label="Фамилия"-->
+<!--            lazy-rules-->
+<!--            :rules="[-->
+<!--              (val) => (val && val.length > 0) || 'Please type something',-->
+<!--            ]"-->
+<!--          />-->
+<!--        </div>-->
+<!--      </q-card-section>-->
+<!--      <q-card-actions align="right" class="text-primary">-->
+<!--        <q-btn-->
+<!--          style="background: #ef7540; color: white; border-radius: 25px"-->
+<!--          flat-->
+<!--          label="Пригласить"-->
+<!--          @click="onSubmit"-->
+<!--          v-close-popup-->
+<!--        />-->
+<!--      </q-card-actions>-->
+<!--    </q-card>-->
+    <q-card style="border: 1px solid gray" class="my-card" flat bordered>
       <q-card-section>
         <div class="text-h6">{{ page?.page.title }}</div>
       </q-card-section>
       <q-markup-table>
         <thead>
           <tr>
-            <th class="text-left">Имя</th>
-            <th class="text-left">Фамилия</th>
-            <th class="text-left">Почта</th>
+            <th style="border: 1px solid gray" class="text-left">Имя</th>
+            <th style="border: 1px solid gray" class="text-left">Фамилия</th>
+            <th style="border: 1px solid gray" class="text-left">Почта</th>
           </tr>
         </thead>
         <tbody v-for="user in tableUsers" :key="user.id">
           <tr>
-            <td>{{ user.fullname.first_name }}</td>
-            <td>{{ user.fullname.last_name }}</td>
-            <td>{{ user.email.email }}</td>
+            <td style="border: 1px solid gray">{{ user.fullname.first_name }}</td>
+            <td style="border: 1px solid gray">{{ user.fullname.last_name }}</td>
+            <td style="border: 1px solid gray">{{ user.email.email }}</td>
           </tr>
         </tbody>
       </q-markup-table>
@@ -95,7 +126,7 @@ const id = ref("");
 
 const inviteUser = ref(false);
 const route = useRoute();
-
+const prompt = ref(false);
 const defId = () => {
   id.value = route.params.id;
 };
