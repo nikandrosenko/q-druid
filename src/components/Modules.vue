f76b20521bf19f3a4b3615077a24b2813af55925
Paronim <Zreba.v@gmial.com>
2023-04-11 14:09:35 +0300

    Форма вынесена в отдельный компанент

--- src/components/Modules.vue
+++ src/components/Modules.vue
@@ -1,169 +1,8 @@
 <template>
-  <q-dialog v-model="prompt">
-    <q-card style="min-width: 350px">
-      <q-card-section>
-        <div v-if="updateDeleteType.bool" class="text-h6">Создать модуль</div>
-        <div v-else class="text-h6">Изменить модуль</div>
-      </q-card-section>
+    <q-dialog v-model="prompt">
+      <Form :dataUpdate="dataUpdate" :updateDeleteType="updateDeleteType" @Create="moduleCreate" @Update="moduleUpdateElement"/>
+    </q-dialog>
 
-      <form @submit.prevent="moduleCreate" v-if="updateDeleteType.bool">
-        <q-input
-        filled
-        v-model="moduleName"
-        label="Имя модуля"
-        class="q-pa-md"
-        :rules="[required]"
-        />
-        <q-select
-          class="q-pa-md"
-          filled
-          v-model="modelUserModule"
-          :options="groupSubjectUsers"
-          label="Ответственный"
-          :rules="[requiredSelect]"
-        />
-
-          <div class="q-pt-md q-px-md" style="max-width: 500px">
-            <q-input readonly filled v-model="dateAndTimeCreateStart" label="Начало" :rules="[required]">
-              <template v-slot:prepend>
-                <q-icon name="event" class="cursor-pointer">
-                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-date v-model="dateCreate.dateCreateStart" mask="DD.MM.YYYY">
-                      <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                      </div>
-                    </q-date>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-
-              <template v-slot:append>
-                <q-icon name="access_time" class="cursor-pointer">
-                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-time v-model="dateCreate.timeCreateStart" mask="HH:mm:ss" format24h>
-                      <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                      </div>
-                    </q-time>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-            </q-input>
-          </div>
-
-          <div class="q-pt-md q-px-md" style="max-width: 500px">
-            <q-input readonly filled v-model="dateAndTimeCreateEnd" label="Конец" :rules="[required]">
-              <template v-slot:prepend>
-                <q-icon name="event" class="cursor-pointer">
-                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-date v-model="dateCreate.dateCreateEnd" mask="DD.MM.YYYY">
-                      <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                      </div>
-                    </q-date>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-
-              <template v-slot:append>
-                <q-icon name="access_time" class="cursor-pointer">
-                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-time v-model="dateCreate.timeCreateEnd" mask="HH:mm:ss" format24h>
-                      <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                      </div>
-                    </q-time>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-            </q-input>
-          </div>
-
-        <q-card-actions align="right" class="text-primary">
-          <q-btn flat label="Отмена" v-close-popup />
-          <q-btn flat label="Создать" v-close-popup type="submit" />
-        </q-card-actions>
-      </form>
-      <form @submit.prevent="moduleUpdateElement(updateDeleteType.id)" v-else>
-        <q-input
-        filled
-        v-model="moduleNameUpdate"
-        label="Имя модуля"
-        class="q-pa-md"
-        :rules="[required]"
-        />
-        <q-select
-          class="q-pa-md"
-          filled
-          v-model="modelUserModuleUpdate"
-          :options="groupSubjectUsers"
-          label="Ответственный"
-          :rules="[requiredSelect]"
-        />
-
-        <div class="q-pt-md q-px-md" style="max-width: 500px">
-            <q-input readonly filled v-model="dateAndTimeUpdateStart" label="Начало" :rules="[required]">
-              <template v-slot:prepend>
-                <q-icon name="event" class="cursor-pointer">
-                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-date v-model="dateUpdate.dateUpdateStart" mask="DD.MM.YYYY">
-                      <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                      </div>
-                    </q-date>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-
-              <template v-slot:append>
-                <q-icon name="access_time" class="cursor-pointer">
-                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-time v-model="dateUpdate.timeUpdateStart" mask="HH:mm:ss" format24h>
-                      <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                      </div>
-                    </q-time>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-            </q-input>
-          </div>
-
-          <div class="q-pt-md q-px-md" style="max-width: 500px">
-            <q-input readonly filled v-model="dateAndTimeUpdateEnd" label="Конец" :rules="[required]">
-              <template v-slot:prepend>
-                <q-icon name="event" class="cursor-pointer">
-                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-date v-model="dateUpdate.dateUpdateEnd" mask="DD.MM.YYYY">
-                      <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                      </div>
-                    </q-date>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-
-              <template v-slot:append>
-                <q-icon name="access_time" class="cursor-pointer">
-                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-time v-model="dateUpdate.timeUpdateEnd" mask="HH:mm:ss" format24h>
-                      <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                      </div>
-                    </q-time>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-            </q-input>
-          </div>
-
-        <q-card-actions align="right" class="text-primary">
-          <q-btn flat label="Отмена" v-close-popup />
-          <q-btn flat label="Изменить" v-close-popup type="submit" />
-        </q-card-actions>
-      </form>
-    </q-card>
-  </q-dialog>
 
   <div class="q-ma-xl">
     <q-btn
@@ -171,8 +10,9 @@
       color="primary"
       @click="
         {
-          prompt = true;
           updateDeleteType.bool = true;
+          moduleUpdateElementForm()
+          prompt = true;
         }
       "
     />
@@ -210,8 +50,8 @@
                   {
                     updateDeleteType.bool = false;
                     updateDeleteType.id = props.row.id;
-                    prompt = true;
                     moduleUpdateElementForm(props.row.index);
+                    prompt = true;
                   }
                 "
                 icon="create"
@@ -229,7 +69,7 @@
 
 <script setup>
 import { useMutation, useQuery } from "@vue/apollo-composable";
-import { computed, ref } from "vue";
+import { ref } from "vue";
 import {
   createModule,
   createPermissionRule,
@@ -242,36 +82,14 @@ import {
 } from "src/graphql/mutations.js";
 import {
   getModulesAll,
-  getGroupSubjects,
   getPagesModule,
 } from "src/graphql/queries.js";
-import { useValidators, useValidatorsSelect } from "src/use/validators.js";
+import Form from "./Form.vue";
 
-const dateCreate = ref({
-  dateCreateEnd: '01.01.2021',
-  dateCreateStart: '01.01.2021',
-  timeCreateStart: '00:00:00',
-  timeCreateEnd: '00:00:00',
-})
-
-const dateAndTimeCreateEnd = computed(() => `${dateCreate.value.dateCreateEnd} ${dateCreate.value.timeCreateEnd}`)
-const dateAndTimeCreateStart = computed(() =>`${dateCreate.value.dateCreateStart} ${dateCreate.value.timeCreateStart}`)
-
-const { required } = useValidators();
-const { requiredSelect } = useValidatorsSelect();
 const rows = ref();
-const groupSubjectUsers = ref();
-const updateDeleteType = ref({
-  bool: true,
-  id: "",
-});
+
 const { result, loading, onResult, refetch } = useQuery(getModulesAll);
-const { result: groupSubject, onResult: onResultGetGroup } = useQuery(
-  getGroupSubjects,
-  {
-    group_id: "3163550221139005516",
-  }
-);
+
 
 onResult(() => {
   rows.value = result?.value?.paginate_type1?.data.map((el, i) => ({
@@ -281,20 +99,16 @@ onResult(() => {
   }));
 });
 
-onResultGetGroup(() => {
-  groupSubjectUsers.value = groupSubject?.value?.get_group.subject.map((el) => {
-    return {
-      label: `${el.fullname.first_name} ${el.fullname.last_name}`,
-      value: el.id,
-    };
-  });
-});
+const updateDeleteType = ref({
+    bool: true,
+    id: "",
+    })
+
+const dataUpdate = ref({
+
+  })
 
 const prompt = ref(false);
-const moduleName = ref("");
-const modelUserModule = ref("");
-const moduleNameUpdate = ref("");
-const modelUserModuleUpdate = ref("");
 
 const columns = [
   {
@@ -324,21 +138,24 @@ const columns = [
   },
 ];
 
-const moduleCreate = async () => {
+const moduleCreate = async (emitValue) => {
+
+  console.log(emitValue.emitValue.date.value)
+
   const { mutate: creatingModule } = useMutation(createModule);
   const { data: createdModule } = await creatingModule({
     input: {
-      name: moduleName.value,
+      name: emitValue.emitValue.moduleName.value,
       property5: {
-        "8044196206941661177": modelUserModule.value.value,
+        "8044196206941661177": emitValue.emitValue.modelUserModule.value.value,
       },
       property6: {
-        date: dateCreate.value.dateCreateStart,
-        time: dateCreate.value.timeCreateStart,
+        date: emitValue.emitValue.date.value.dateStart,
+        time: emitValue.emitValue.date.value.timeStart,
       },
       property7: {
-        date: dateCreate.value.dateCreateEnd,
-        time: dateCreate.value.timeCreateEnd,
+        date: emitValue.emitValue.date.value.dateEnd,
+        time: emitValue.emitValue.date.value.timeEnd,
       },
     },
   });
@@ -362,7 +179,7 @@ const moduleCreate = async () => {
       model_type: "page",
       model_id: createdPage.pageCreate.recordId,
       owner_type: "subject",
-      owner_id: modelUserModule.value.value,
+      owner_id: emitValue.emitValue.modelUserModule.value.value,
       level: 5,
     },
   });
@@ -373,7 +190,7 @@ const moduleCreate = async () => {
         model_type: "object",
         model_id: createdModule.create_type1.recordId,
         owner_type: "subject",
-        owner_id: modelUserModule.value.value,
+        owner_id: emitValue.emitValue.modelUserModule.value.value,
         level: 5,
       },
     });
@@ -400,7 +217,10 @@ const moduleDelete = async (moduleId, pageId) => {
     page_id: pageId,
   });
 
-  console.log(delM, delP);
+  return{
+    delM,
+    delP
+  }
 };
 
 const delModule = ref();
@@ -425,29 +245,29 @@ const { mutate: updatingPermissionRule } = useMutation(updatePermissionRule)
 
 const updatedModule = ref();
 
-const moduleUpdate = async (moduleId, pageId) => {
+const moduleUpdate = async (moduleId, pageId, emitValue) => {
   const { data: updateM } = await updatingModule({
     id: moduleId,
     input: {
-      name: moduleNameUpdate.value,
+      name: emitValue.emitValue.moduleName.value,
       property5: {
-        "8044196206941661177": modelUserModuleUpdate.value.value,
+        "8044196206941661177": emitValue.emitValue.modelUserModule.value.value,
       },
       property6: {
-        date: dateUpdate.value.dateUpdateStart,
-        time: dateUpdate.value.timeUpdateStart,
+        date: emitValue.emitValue.date.value.dateStart,
+        time: emitValue.emitValue.date.value.timeStart,
       },
       property7: {
-        date: dateUpdate.value.dateUpdateEnd,
-        time: dateUpdate.value.timeUpdateEnd,
+        date: emitValue.emitValue.date.value.dateEnd,
+        time: emitValue.emitValue.date.value.timeEnd,
       },
     },
   });
-  console.log(moduleNameUpdate.value);
+
   const { data: updateP } = await updatingPage({
     id: pageId,
     input: {
-      title: moduleNameUpdate.value,
+      title: emitValue.emitValue.moduleName.value,
     },
   });
 
@@ -456,32 +276,44 @@ const moduleUpdate = async (moduleId, pageId) => {
   refetch();
 };
 
-const dateUpdate = ref()
-
-const dateAndTimeUpdateEnd = computed(() => `${dateUpdate.value.dateUpdateEnd} ${dateUpdate.value.timeUpdateEnd}`)
-const dateAndTimeUpdateStart = computed(() =>`${dateUpdate.value.dateUpdateStart} ${dateUpdate.value.timeUpdateStart}`)
-
 const moduleUpdateElementForm = (index) => {
-  dateUpdate.value = {
-  dateUpdateEnd: rows.value[index].property7.date,
-  dateUpdateStart: rows.value[index].property6.date,
-  timeUpdateStart: rows.value[index].property6.time,
-  timeUpdateEnd: rows.value[index].property7.time,
+
+  if(updateDeleteType.value.bool){
+    dataUpdate.value = {
+    dateUpdate: {
+      dateUpdateEnd: '01.01.2021',
+      dateUpdateStart: '01.01.2021',
+      timeUpdateStart: '00:00:00',
+      timeUpdateEnd: '00:00:00',
+    },
+
+    moduleNameUpdate: "",
+    modelUserModuleUpdate: ""
+  }
+  } else {
+    dataUpdate.value = {
+    dateUpdate: {
+      dateUpdateEnd: rows.value[index].property7.date,
+      dateUpdateStart: rows.value[index].property6.date,
+      timeUpdateStart: rows.value[index].property6.time,
+      timeUpdateEnd: rows.value[index].property7.time,
+    },
+
+    moduleNameUpdate: rows.value[index].name,
+    modelUserModuleUpdate: {
+      label: `${rows.value[index].property5.fullname.first_name} ${rows.value[index].property5.fullname.last_name}`,
+      value: rows.value[index].property5.id,
+    }
+  }
   }
 
-  moduleNameUpdate.value = rows.value[index].name;
-  modelUserModuleUpdate.value = {
-    label: `${rows.value[index].property5.fullname.first_name} ${rows.value[index].property5.fullname.last_name}`,
-    value: rows.value[index].property5.id,
-  };
 };
 
-const moduleUpdateElement = (id) => {
+const moduleUpdateElement = (emitValue) => {
   updatedModule.value = pageData?.value?.page.children.data.find(
-    (el) => el.object.id == id
+    (el) => el.object.id == emitValue.emitValue.id
   );
-  console.log(id);
 
-  moduleUpdate(id, updatedModule.value.id);
+  moduleUpdate(emitValue.emitValue.id, updatedModule.value.id, emitValue);
 };
 </script>
