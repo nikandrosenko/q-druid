c3d942ae53f2850f4246585129d6b4f1a086129b
Nikita Androsenko <varzaid@gmail.com>
2023-04-21 13:14:54 +0300

    Удаление комментариев

--- src/components/FormTasks.vue
+++ src/components/FormTasks.vue
@@ -39,10 +39,6 @@ const statusTaskList = [
     label: "Выполнено",
     value: process.env.COMPLETED_ID,
   },
-  // {
-  //   label: "Завершена",
-  //   value: process.env.FINISHED_ID,
-  // },
 ];
 const props = defineProps({
   updateData: Object,
